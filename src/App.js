ed8dff929c3c22c0d8c33e27a2caf044215f885c
AnilP. <anl.pirincoglu@gmail.com>
2023-11-21 22:46:25 +0300

    tamamlandı

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@
 */
 
 // State hook u import edin
-import React from "react";
+import React, { useState } from "react";
 
 // Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
 // sahteVeri'yi import edin
 import "./App.css";
+import Gonderiler from "./bilesenler/Gonderiler/Gonderiler";
+import AramaCubugu from "./bilesenler/AramaCubugu/AramaCubugu";
+import sahteVeri from "./sahte-veri";
 
 const App = () => {
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
-
+  const [gonderiler, setGonderiler] = useState(sahteVeri);
+  const [search, setSearch] = useState("");
+  const [begeniListesi, setBegeniListesi] = useState([]);
   const gonderiyiBegen = (gonderiID) => {
     /*
       Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
@@ -23,18 +28,27 @@ const App = () => {
       Uygulamanın durumu, React ağacının en üstünde bulunur, ancak iç içe geçmiş bileşenlerin stateleri değiştirememesi adil olmaz!
       Bu fonksiyon, belirli bir gönderinin beğeni sayısını artırılmasına olanak sağlamak amacıyla iç içe geçmiş bileşenlere aktarılır.
 
-	  "setGonderi" yi çağırın ve state ine "posts.map" çağrısını iletin.
+    "setGonderi" yi çağırın ve state ine "posts.map" çağrısını iletin.
       `map` içine iletilen callback aşağıdaki mantığı gerçekleştirir:
         - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
         - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
      */
+    const guncelGonderiler = gonderiler.map((post) => {
+      if (post.id === gonderiID && !begeniListesi.includes(gonderiID)) {
+        post.likes++;
+        setBegeniListesi([...begeniListesi, gonderiID]);
+      }
+      return post;
+    });
+    setGonderiler(guncelGonderiler);
   };
-
+  const filterGonderiler = gonderiler.filter((gonderi) => gonderi.username.includes(search) || gonderi.timestamp.includes(search));
   return (
     <div className="App">
-      App Çalışıyor
       {/* Yukarıdaki metni projeye başladığınızda silin*/}
       {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
+      <AramaCubugu search={search} setSearch={setSearch} />
+      <Gonderiler gonderiler={filterGonderiler} gonderiyiBegen={gonderiyiBegen} />
       {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
     </div>
   );
--- src/bilesenler/Gonderiler/Gonderiler.js
+++ src/bilesenler/Gonderiler/Gonderiler.js
@@ -8,7 +8,9 @@ const Gonderiler = (props) => {
 
   return (
     <div className="posts-container-wrapper">
-      {/* gönderiler dizisini işleyip her döngüde bir Gönderi bileşeni çağırın*/}
+      {/* gönderiler dizisini işleyip her döngüde bir Gönderi bileşeni çağırın*/
+        gonderiler.map((post) => <Gonderi key={post.id} gonderi={post} gonderiyiBegen={gonderiyiBegen} />)
+      }
       {/* Gönderi'nin çağırılmasında hangi propları kullanmanız gerektiğine dikkat edin! */}
     </div>
   );
